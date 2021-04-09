dfad1caebb7952117a3d051760bd2573ddcf28a0
Abdelaali Es salmi <aes-salm@e1r10p6.1337.ma>
2021-04-09 16:34:53 +0100

    finish login funct and home page

--- center_GAS/urls.py
+++ center_GAS/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from .views import home_page
 
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', home_page, name="home_page"),
+    path('handicapped/', include('handicapped.urls')),
+    path('admin/', admin.site.urls),
 ]
--- center_GAS/views.py
+++ center_GAS/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as login_user
 
 
 def home_page(request):
     template_name = 'home_page.html'
+    if request.method == 'POST':
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+        if username and password:
+            user = authenticate(
+                username = username,
+                password = password
+            )
+            if user:
+                login_user(request, user)
+                messages.success(request, 'لقد تم تسجيل الدخول بنجاح')
+            else:
+                messages.error(request, 'إسم المستخدم أو كلمة المرور  غير صحيحة')
+        else:
+            messages.error(request, 'الرجاء إدخال اسم المستخدم وكلمة المرور')
     return render(request, template_name, {})
+
